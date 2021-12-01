952ba4fe468224a5936257d3966d48090f29d4bd
James Calnan <james.calnan@outlook.com>
2021-12-01 14:13:26 +0000

    Initial commit

---
+++ day01/main.py
@@ -0,0 +1,14 @@
+from rich.console import Console
+
+c = Console()
+
+def part1():
+    file = [int(x) for x in open("input.txt", "r").read().split("\n")]
+    return len([x for x in range(len(file)) if file[x] > file[x - 1]])
+
+def part2():
+    file = [int(x) for x in open("input.txt", "r").read().split("\n")]
+    return (len([x for x in range(len(file)) if sum(file[x:x+3]) < sum(file[x+1:x+4])]))
+
+c.print(part1())
+c.print(part2())
