aeef95d8faac276ef58d5c0a84d361f23b9775b6
James Calnan <james.calnan@outlook.com>
2021-12-01 14:14:21 +0000

    Update main.py

--- day01/main.py
+++ day01/main.py
@@ -1,10 +1,7 @@
-from rich.console import Console
-
-c = Console()
-
 def part1():
     file = [int(x) for x in open("input.txt", "r").read().split("\n")]
     return len([x for x in range(len(file)) if file[x] > file[x - 1]])
+
 
 def part2():
     file = [int(x) for x in open("input.txt", "r").read().split("\n")]
